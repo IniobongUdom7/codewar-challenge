b477a48ff2af4f5c227459e844afc0f6835c513c
udom <udompowerini@gmail.com>
2022-11-14 14:54:58 +0100

    string to number

--- 8KYU/string2num.js
+++ 8KYU/string2num.js
@@ -2,3 +2,9 @@ function numberToString(num) {
     // Return a string of the number here!
     return num.toString();
   }
+
+  const stringToNumber = function(str){
+  
+    // put your code here
+    return Number(str)
+  }
